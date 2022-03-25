67be94017d46fab97dbec6108da8483fe652834f
roman.limonov <roman.limonov@interexy.com>
2022-03-25 16:09:12 +0300

    work on 3d carousel logic

--- src/components/3dMainpageCarousel/Mainpage3dCarousel.jsx
+++ src/components/3dMainpageCarousel/Mainpage3dCarousel.jsx
@@ -3,29 +3,35 @@ import cn from "classnames";
 import { SpecialAuctionNFTCard } from "../SpecialAuctionNFTCard/SpecialAuctionNFTCard";
 import styles from "./Mainpage3dCarousel.module.css";
 
+// this component is still in process
+
 export const Mainpage3dCarousel = ({ NFTCards }) => {
   const [activeI, setactiveI] = useState(2);
+  const [prevI, setPrevI] = useState(2);
   const [translation, setTranslation] = useState(0);
 
   const handleClick = (i) => {
     const difference = activeI - i;
-
-    setTranslation((prev) => prev + difference * 100);
     setactiveI(i);
-    console.log('activeI', activeI)
+
+    setTranslation((prev) => prev + difference * 388);
+    console.log("activeI", activeI);
   };
 
   const getRotation = (i) => {
+    console.log("getRotation");
+    console.log("---activeI", activeI);
+    console.log("---i", i);
     const degree = 20;
 
     if (i < activeI) {
       const difference = activeI - i;
-      return `rotateY(${degree * (difference)}deg)`;
+      return `rotateY(${degree * difference}deg)`;
     }
 
     if (i > activeI) {
       const difference = i - activeI;
-      return `rotateY(-${degree * (difference)}deg)`;
+      return `rotateY(-${degree * difference}deg)`;
     }
 
     return "";
@@ -43,13 +49,14 @@ export const Mainpage3dCarousel = ({ NFTCards }) => {
     return NFTCards.length;
   };
 
-
   return (
     <div className={styles.wrapper}>
       {NFTCards.map((card, i) => (
         <SpecialAuctionNFTCard
           styling={{
-            transform: `perspective(500px) translateZ(${getZIndex(i) * 20}px) translateX(${translation}%) ${getRotation(i)}`,
+            transform: `perspective(500px) translateZ(${
+              getZIndex(i)
+            }px) translateX(${translation}px) ${getRotation(i)}`,
             transition: "0.3s",
             zIndex: `${Math.abs(getZIndex(i))}`,
           }}
