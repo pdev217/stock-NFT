e84a363a9d228435615af3ff0b99c4c9672b2b39
roman.limonov <roman.limonov@interexy.com>
2022-03-28 13:17:24 +0300

    done scrollable with rotation

--- src/components/3dMainpageCarousel/Mainpage3dCarousel.jsx
+++ src/components/3dMainpageCarousel/Mainpage3dCarousel.jsx
@@ -1,79 +1,115 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import cn from "classnames";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
 import { SpecialAuctionNFTCard } from "../SpecialAuctionNFTCard/SpecialAuctionNFTCard";
 import styles from "./Mainpage3dCarousel.module.css";
+import { useEffect } from "react";
 
 // this component is still in process
 
 export const Mainpage3dCarousel = ({ NFTCards }) => {
-  const [activeI, setactiveI] = useState(2);
-  const [prevI, setPrevI] = useState(2);
-  const [translation, setTranslation] = useState(0);
-
-  const handleClick = (i) => {
-    const difference = activeI - i;
-    setactiveI(i);
-
-    setTranslation((prev) => prev + difference * 388);
-    console.log("activeI", activeI);
-  };
-
-  const getRotation = (i) => {
-    console.log("getRotation");
-    console.log("---activeI", activeI);
-    console.log("---i", i);
-    const degree = 20;
-
-    if (i < activeI) {
-      const difference = activeI - i;
-      return `rotateY(${degree * difference}deg)`;
-    }
-
-    if (i > activeI) {
-      const difference = i - activeI;
-      return `rotateY(-${degree * difference}deg)`;
-    }
-
-    return "";
-  };
-
-  const getZIndex = (i) => {
-    if (i > activeI) {
-      return NFTCards.length - (i - activeI);
-    }
-
-    if (i < activeI) {
-      return NFTCards.length - (activeI - i);
-    }
-
-    return NFTCards.length;
-  };
-
   return (
-    <div className={styles.wrapper}>
-      {NFTCards.map((card, i) => (
-        <SpecialAuctionNFTCard
-          styling={{
-            transform: `perspective(500px) translateZ(${
-              getZIndex(i)
-            }px) translateX(${translation}px) ${getRotation(i)}`,
-            transition: "0.3s",
-            zIndex: `${Math.abs(getZIndex(i))}`,
-          }}
-          onClick={() => handleClick(i)}
-          NFTName={card.NFTName}
-          time={card.time}
-          username={card.username}
-          code={card.code}
-          currPrice={card.currPrice}
-          prevPrices={card.prevPrices}
-          chartData={card.chartData}
-          views={card.views}
-          src={card.src}
-          tag={card.tag}
-          key={card.id}
-        />
-      ))}
-    </div>
+    <>
+    <div className={styles.empty}></div>
+      <div className={styles.wrapper}>
+        {NFTCards.map((card, i) => (
+          <SpecialAuctionNFTCard
+            key={card.id}
+            NFTName={card.NFTName}
+            time={card.time}
+            username={card.username}
+            code={card.code}
+            currPrice={card.currPrice}
+            prevPrices={card.prevPrices}
+            chartData={card.chartData}
+            views={card.views}
+            src={card.src}
+            tag={card.tag}
+          />
+        ))}
+      </div>
+      <div className={styles.empty}></div>
+
+    </>
   );
+
+  /// previous version of implementation
+
+  // const [activeI, setactiveI] = useState(2);
+  // const [prevI, setPrevI] = useState(2);
+  // const [translation, setTranslation] = useState(0);
+
+  // const handleClick = (i) => {
+  //   const difference = activeI - i;
+  //   setactiveI(i);
+
+  //   setTranslation((prev) => prev + difference * 388);
+  //   // console.log("activeI", activeI);
+  // };
+
+  // const getPerspective = (i) => {
+  //   const baseValue = 1000;
+  //   const difference = i < activeI ? activeI - i : i - activeI;
+
+  //   return `${baseValue * difference + baseValue}`;
+  // };
+
+  // const getRotation = (i) => {
+  //   console.log("getRotation");
+  //   console.log("---activeI", activeI);
+  //   console.log("---i", i);
+  //   const degree = 20;
+
+  //   if (i < activeI) {
+  //     const difference = activeI - i;
+  //     return `rotateY(${degree * difference}deg)`;
+  //   }
+
+  //   if (i > activeI) {
+  //     const difference = i - activeI;
+  //     return `rotateY(-${degree * difference}deg)`;
+  //   }
+
+  //   return "";
+  // };
+
+  // const getZIndex = (i) => {
+  //   if (i > activeI) {
+  //     return NFTCards.length - (i - activeI);
+  //   }
+
+  //   if (i < activeI) {
+  //     return NFTCards.length - (activeI - i);
+  //   }
+
+  //   return NFTCards.length;
+  // };
+
+  // return (
+  //   <div className={styles.wrapper}>
+  //     {NFTCards.map((card, i) => (
+  //       <SpecialAuctionNFTCard
+  //         styling={{
+  //           transform: `scaleX(1) perspective(${getPerspective(i)}px) translateZ(${getZIndex(
+  //             i
+  //           )}px) translateX(${translation}px) ${getRotation(i)}`,
+  //           transition: "0.3s",
+  //           zIndex: `${Math.abs(getZIndex(i))}`,
+  //         }}
+  //         onClick={() => handleClick(i)}
+  //         NFTName={card.NFTName}
+  //         time={card.time}
+  //         username={card.username}
+  //         code={card.code}
+  //         currPrice={card.currPrice}
+  //         prevPrices={card.prevPrices}
+  //         chartData={card.chartData}
+  //         views={card.views}
+  //         src={card.src}
+  //         tag={card.tag}
+  //         key={card.id}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 };
--- src/components/SpecialAuctionNFTCard/SpecialAuctionNFTCard.jsx
+++ src/components/SpecialAuctionNFTCard/SpecialAuctionNFTCard.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import cn from "classnames";
 import Image from "next/image";
 import { AmountWithIcon } from "../AmountWithIcon/AmountWithIcon";
 import { Tag } from "../Tag/Tag";
 import { SmallChart } from "../SmallChart/SmallChart";
 import { CustButton } from "../CustButton/CustButton";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
 import { Views } from "../Views/Views";
 import styles from "./SpecialAuctionNFTCard.module.css";
 
@@ -21,12 +22,44 @@ export const SpecialAuctionNFTCard = ({
   views,
   src,
   tag,
-  styling
+  styling,
 }) => {
   const [isImageAbsent, setIsImageAbsent] = useState(false);
+  const [rotation, setRotation] = useState(0);
+  const ref = useRef();
+  const windowWidth = useWindowWidth();
+  const windowCenter = windowWidth / 2;
+  console.log("windowCenter", windowCenter);
+
+  useEffect(() => {
+      const { width } = ref.current.getBoundingClientRect();
+      const {parentNode} = ref.current.parentNode
+      console.log('parentNode',parentNode);
+
+      const handleScroll = () => {
+        const { x } = ref.current.getBoundingClientRect();
+
+        const viewportCenter = windowCenter === 0 ? window.innerWidth / 2 : windowCenter;
+        const elementCenterPosition = x + width / 2;
+        const distance = viewportCenter - elementCenterPosition;
+
+        const baseDegree = 20;
+        const degree = (distance / width) * baseDegree;
+        setRotation(degree);
+      };
+      parentNode.addEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div onClick={onClick} className={cn(styles.wrapper, className)} style={{...styling}}>
+    <div
+      ref={ref}
+      onClick={onClick}
+      className={cn(styles.wrapper, className)}
+      style={{
+        transform: `perspective(500px) translateZ(${-Math.abs(rotation * 2)}px) rotateY(${rotation}deg)`,
+        transition: "0.3s",
+      }}
+    >
       <div className={styles.leftSide}>
         <div className={styles.imageWrapper}>
           {isImageAbsent ? (
@@ -55,12 +88,7 @@ export const SpecialAuctionNFTCard = ({
       </div>
       <div className={styles.rightSide}>
         <p className={styles.currentPriceTitle}>CURRENT PRICE</p>
-        <AmountWithIcon
-          amount={currPrice}
-          color="primary"
-          size="l"
-          className={styles.amountWithIconBig}
-        />
+        <AmountWithIcon amount={currPrice} color="primary" size="l" className={styles.amountWithIconBig} />
         <Views quantity={views} className={styles.views} />
         <p className={styles.historyTitle}>HISTORY</p>
         <SmallChart data={chartData} className={styles.chart} />
