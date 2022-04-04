ab2ece3c70d4638b21c14164d39f9fa464f4270e
roman.limonov <roman.limonov@interexy.com>
2022-04-04 09:19:02 +0300

    six index js in mainpage

--- src/page-components/MainPage/index.js
+++ src/page-components/MainPage/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import { useDispatch } from "react-redux";
 import { open as openError } from "../../redux/slices/errorSnackbarSlice";
-=======
->>>>>>> 472e6f6e5fd30e016e37770be94344a938014b5d
 //page-sections
 import { Collections } from "./Collections/Collections";
 import { HottestNFTCollectibles } from "./HottestNFTCollectibles/HottestNFTCollectibles";
@@ -16,11 +13,8 @@ import { VideoAboutNFT } from "./VideoAboutNFT/VideoAboutNFT";
 import { TopCollections } from "./TopCollections/TopCollections";
 import { JoinOurCreatoes } from "./JoinOurCreators/JoinOurCreatoes";
 import { PopularCreators } from "./PopularCreators/PopularCreators";
-<<<<<<< HEAD
 //hooks
 import useAuth from "../../hooks/useAuth";
-=======
->>>>>>> 472e6f6e5fd30e016e37770be94344a938014b5d
 
 export const MainPage = () => {
   const dispatch = useDispatch();
