92a7f955b0db3f66add11499d9dd98443c3c1b9e
roman.limonov <roman.limonov@interexy.com>
2022-04-27 09:08:44 +0300

    put create collection inside my-collections

--- src/redux/slices/adminSlice.js
+++ src/redux/slices/adminSlice.js
@@ -1,26 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    fees: {
-        stokeFee: 2.5,
-        creatorRoyalty: 10,
-    },
-    pagesForUnauthorized: [
-        '/', '/connect-wallet', '/token/[tokenId]', '/create-nft', '/my-collections'
-    ]
+  fees: {
+    stokeFee: 2.5,
+    creatorRoyalty: 10,
+  },
+  pagesForUnauthorized: [
+    "/",
+    "/connect-wallet",
+    "/token/[tokenId]",
+    "/create-nft",
+    "/my-collections/create-collection",
+  ],
 };
 
 export const administration = createSlice({
-    name: 'administration',
-    initialState,
-    reducers: {
-        setFees: (state, action) => {
-            state.fees = {...action.payload}
-        },
-        setPagesForUnauthorized: (state, action) => {
-            state.pagesForUnauthorized = [...action.payload]
-        }
-    }
+  name: "administration",
+  initialState,
+  reducers: {
+    setFees: (state, action) => {
+      state.fees = { ...action.payload };
+    },
+    setPagesForUnauthorized: (state, action) => {
+      state.pagesForUnauthorized = [...action.payload];
+    },
+  },
 });
 
 export const { setFees, setPagesForUnauthorized } = administration.actions;
