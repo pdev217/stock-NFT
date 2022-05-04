fc1f40c7307087178f8c2cb61fd89e3e502686f8
roman.limonov <roman.limonov@interexy.com>
2022-05-04 13:08:36 +0300

    initialized list price page

--- pages/token/[tokenId]/index.js
+++ pages/token/[tokenId]/index.js
@@ -1,10 +1,8 @@
 import { ViewIndividualTokenPage } from "../../../src/page-components/ViewIndividualTokenPage/ViewIndividualTokenPage";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
 import https from "https";
 import axios from "axios";
 import { withLayout } from "../../../layout/Layout";
-import { getEtherPrice } from "../../../src/utils";
 
 const ViewIndividualToken = (props) => <ViewIndividualTokenPage {...props} />;
 
@@ -22,28 +20,10 @@ export const getServerSideProps = async ({ params }) => {
   const { data } = await axios.get(`${process.env.BACKEND_URL}/nfts/${params.tokenId}`, {
     httpsAgent,
   });
-  console.log('---data', data)
-
-  const adaptPriceAndType = async (array, type) => {
-    const newArray = Promise.all(array.map(
-      async (elem) =>
-        await getEtherPrice().then((result) => {
-          return { ...elem, usdPrice: `$${(elem.price * result).toFixed(3)}`, type };
-        })
-    ));
-    return newArray;
-  };
-
-  const adaptedOffers = await adaptPriceAndType(data.offers, 'Offers');
 
   return {
     props: {
       ...data,
-      offers: adaptedOffers,
-      user: data.owner,
-      collectionName: data.collection?.name || null,
-      about: data.collection?.description,
-      blockchainName: data.blockchainType?.name || null,
     },
   };
 };
---
+++ src/page-components/ListFixedPage/ListFixedPage.jsx
@@ -0,0 +1,5 @@
+import styles from './ListFixedPage.module.scss';
+
+export const ListFixedPage = () => {
+    return <></>
+}
