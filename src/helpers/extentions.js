6ba138199c5a314b760407e81941fb136bb52fe8
roman.limonov <roman.limonov@interexy.com>
2022-05-11 15:39:40 +0300

    delete stylelint

--- src/helpers/extentions.js
+++ src/helpers/extentions.js
@@ -1,3 +1,9 @@
-export const images = ["svg", "jpg", "png", "gif"];
-export const videos = ["mp4", "webm"];
-export const audios = ["mp3", "wav"];
+export const images = ['svg', 'jpg', 'png', 'gif'];
+export const videos = ['mp4', 'webm'];
+export const audios = ['mp3', 'wav'];
+
+// "stylelint": "^13.13.1",
+// "stylelint-config-standard-scss": "^3.0.0",
+// "stylelint-order": "^4.1.0",
+// "stylelint-order-config-standard": "^0.1.3",
+// "stylelint-scss": "^4.2.0"
