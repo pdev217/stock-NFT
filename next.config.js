ef228972d3e2eb504b226c79833e1037f9f0180b
roman.limonov <roman.limonov@interexy.com>
2022-05-24 14:18:57 +0300

    changed assets url

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ module.exports = {
   env: {
     INFURA_ID: 'a5b51678f5c248c5af6c10dc7c5501ea',
     BACKEND_URL: 'https://dev.api.stokenft.com/api',
-    BACKEND_ASSETS_URL: 'https://dev.api.stokenft.com/assets',
+    BACKEND_ASSETS_URL: 'https://dev.api.stokenft.com',
     PINATA_API_KEY: '1f8e37ec423bce5ed562',
     PINATA_SECRET_API_KEY: '12bc6c92c377a63574ac6307bff4213da8adf58b604148fb3313018948f1b864',
     TOKEN_URL: '0x194194b1D78172446047e327476B811f5D365c21',
